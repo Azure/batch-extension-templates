5a8d73227e0cadc37b8125ba78b8f51332d3117b
David Kydd <david.kydd@Microsoft.com>
2019-12-13 10:57:21 +1300

    Fix typo

--- runner/test_manager.py
+++ runner/test_manager.py
@@ -97,7 +97,7 @@ class TestManager(object):
     def monitor_pool_and_retry_if_needed(self, batch_service_client: batch.BatchExtensionsClient, image_references: 'List[utils.ImageReference]', test_timeout: datetime, stop_thread, VM_image_URL, VM_OS_type):
         try:
             utils.wait_for_steady_nodes(batch_service_client, self.pool_id, self.min_required_vms, test_timeout, stop_thread)
-            self.pool_start_duration = timedelta_since(self.start_time)
+            self.pool_start_duration = utils.timedelta_since(self.start_time)
 
         except (ex.PoolResizeFailedException, ex.NodesFailedToStartException):
             #pool failed to get enough nodes to idle from both initial allocation and any secondary pool resize too - try create a whole new pool and change job to target it
@@ -108,7 +108,7 @@ class TestManager(object):
             utils.retarget_job_to_new_pool(batch_service_client, self.job_id, self.pool_id)
 
             utils.wait_for_steady_nodes(batch_service_client, self.pool_id, self.min_required_vms, test_timeout, stop_thread)
-            self.pool_start_duration = timedelta_since(self.start_time)
+            self.pool_start_duration = utils.timedelta_since(self.start_time)
 
     def monitor_job_and_retry_if_needed(self, batch_service_client: batch.BatchExtensionsClient, test_timeout: datetime, stop_thread):
         try:
