9d4693495676fc2b6d74332dda797cfffcfce030
David Kydd <david.kydd@Microsoft.com>
2020-02-05 14:14:59 +1300

    log running threads before exit

--- runner/runner.py
+++ runner/runner.py
@@ -15,6 +15,7 @@ import azext.batch as batch
 import argparse
 from pygit2 import Repository
 import uuid
+import threading
 
 """
 This python module is used for validating the rendering templates by using the azure CLI. 
@@ -226,6 +227,7 @@ def main():
         end_time = datetime.now(timezone.utc).replace(microsecond=0)
         logger.print_result(_test_managers)
         logger.export_result(_test_managers, (end_time - start_time))
+        
     logger.info('Sample end: {}'.format(end_time))
     logger.info('Elapsed time: {}'.format(end_time - start_time))
 
@@ -233,6 +235,13 @@ if __name__ == '__main__':
     try:
         main()
         logger.info("Exit code 0")
+
+        thread_names = {t.ident: t.name for t in threading.enumerate()}
+        for thread_id, frame in sys._current_frames().items():
+            print("Thread %s:" % thread_names.get(thread_id, thread_id))
+            traceback.print_stack(frame)
+            print()
+
         sys.exit(0)
     except Exception as err:
         traceback.print_exc()
