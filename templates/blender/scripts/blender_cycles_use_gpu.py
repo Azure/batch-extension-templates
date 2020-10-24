b9c407c50501048ea4282e61943928af431b80e8
Martin Seitz <MartinTheMichel@hotmail.de>
2020-10-25 01:33:36 +0200

    Further bug fixes for devices

--- templates/blender/scripts/blender_cycles_use_gpu.py
+++ templates/blender/scripts/blender_cycles_use_gpu.py
@@ -4,18 +4,13 @@ for scene in bpy.data.scenes:
     scene.cycles.device = 'GPU'
     scene.render.engine = 'CYCLES'
 
-
 preferences = bpy.context.preferences
 cycles_preferences = preferences.addons["cycles"].preferences
-devices = cycles_preferences.get_devices()
 
 cycles_preferences.compute_device_type = 'CUDA'
 
-
-print("set scene.cycles.device and scene.render.engine")
-
-
-for d in devices:
+cycles_preferences.get_devices()
+for d in cycles_preferences.devices:
     if d.type == 'CPU':
         d.use = False
     print("Device '{}' type {} : {}" . format(d.name, d.type, d.use))
