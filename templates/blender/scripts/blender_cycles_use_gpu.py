e2f2190b606b944988ebdb15973a2875fe105ea3
Martin Seitz <MartinTheMichel@hotmail.de>
2020-10-25 01:21:34 +0200

    Changed device preferences
    get_devices must be used before setting can be done

--- templates/blender/scripts/blender_cycles_use_gpu.py
+++ templates/blender/scripts/blender_cycles_use_gpu.py
@@ -2,10 +2,22 @@ import bpy
 
 for scene in bpy.data.scenes:
     scene.cycles.device = 'GPU'
+    scene.render.engine = 'CYCLES'
 
-bpy.context.preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
 
-for d in bpy.context.preferences.addons['cycles'].preferences.devices:
+preferences = bpy.context.preferences
+cycles_preferences = preferences.addons["cycles"].preferences
+devices = cycles_preferences.get_devices()
+
+cycles_preferences.compute_device_type = 'CUDA'
+
+
+print("set scene.cycles.device and scene.render.engine")
+
+
+for d in devices:
     if d.type == 'CPU':
         d.use = False
     print("Device '{}' type {} : {}" . format(d.name, d.type, d.use))
+
+
