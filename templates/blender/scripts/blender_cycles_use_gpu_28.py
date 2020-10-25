31f8c87b182659a8d7e8b02214787b74bfa9b634
Martin Seitz <MartinTheMichel@hotmail.de>
2020-10-25 09:37:26 +0100

    Create blender 2.8 file

---
+++ templates/blender/scripts/blender_cycles_use_gpu_28.py
@@ -0,0 +1,16 @@
+import bpy
+
+for scene in bpy.data.scenes:
+    scene.cycles.device = 'GPU'
+    scene.render.engine = 'CYCLES'
+
+preferences = bpy.context.preferences
+cycles_preferences = preferences.addons["cycles"].preferences
+
+cycles_preferences.compute_device_type = 'CUDA'
+
+cycles_preferences.get_devices()
+for d in cycles_preferences.devices:
+    if d.type == 'CPU':
+        d.use = False
+    print("Device '{}' type {} : {}" . format(d.name, d.type, d.use))
