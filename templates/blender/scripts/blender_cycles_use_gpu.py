512232cb89f97e480a8fdc5ba4ad9a1c85c72312
Martin Seitz <MartinTheMichel@hotmail.de>
2020-10-25 09:38:24 +0100

    Updated blender file to contain user_preferences

--- templates/blender/scripts/blender_cycles_use_gpu.py
+++ templates/blender/scripts/blender_cycles_use_gpu.py
@@ -4,7 +4,7 @@ for scene in bpy.data.scenes:
     scene.cycles.device = 'GPU'
     scene.render.engine = 'CYCLES'
 
-preferences = bpy.context.preferences
+preferences = bpy.context.user_preferences
 cycles_preferences = preferences.addons["cycles"].preferences
 
 cycles_preferences.compute_device_type = 'CUDA'
