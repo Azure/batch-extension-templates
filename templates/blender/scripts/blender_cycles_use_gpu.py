2ea781e93579dc9ce56d22bdec3550119cc8793b
Martin Seitz <MartinTheMichel@hotmail.de>
2020-10-27 17:22:57 +0100

    Added get_devices and tile size changes for gpu rendering
    This python script needs to be called aftre the file is loaded. Within the command line use "-P" parameter and this script after the ".blend" file name. Th eorder on the command line in blender.ex is important

--- templates/blender/scripts/blender_cycles_use_gpu.py
+++ templates/blender/scripts/blender_cycles_use_gpu.py
@@ -1,18 +1,25 @@
 import bpy
 
-for scene in bpy.data.scenes:
-    scene.cycles.device = 'GPU'
-    scene.render.engine = 'CYCLES'
-
-preferences = bpy.context.user_preferences
+print("Loaded: ", bpy.data.filepath)
+print(bpy.app.version_string)
+ 
+preferences = bpy.context.preferences
 cycles_preferences = preferences.addons["cycles"].preferences
-
-cycles_preferences.compute_device_type = 'CUDA'
-
+ 
 cycles_preferences.get_devices()
 for d in cycles_preferences.devices:
     if d.type == 'CPU':
         d.use = False
     print("Device '{}' type {} : {}" . format(d.name, d.type, d.use))
+ 
+cycles_preferences.compute_device_type = 'CUDA'
+ 
+for scene in bpy.data.scenes:
+    scene.render.tile_x = 256
+    scene.render.tile_y = 256
+    scene.render.engine = 'CYCLES'
+    scene.cycles.device = 'GPU'
+
+
 
 
