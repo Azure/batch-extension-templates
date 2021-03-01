6f1423aa6b88557a74d50a1b25104a073a358761
david kydd <david.kydd@microsoft.com>
2021-03-01 16:05:46 +1300

    MAYBE REVERT - rolled back some changes to see if just adding the /wait back in was enough

--- templates/3dsmax/scripts/PrepareVrayServices.bat
+++ templates/3dsmax/scripts/PrepareVrayServices.bat
@@ -3,8 +3,8 @@ set VRAY_RENDERER=%2
 set VRAY_PORT=%3
 
 setx AZ_BATCH_ACCOUNT_URL %AZ_BATCH_ACCOUNT_URL% /M
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v "AZ_BATCH_SOFTWARE_ENTITLEMENT_TOKEN" /d %AZ_BATCH_SOFTWARE_ENTITLEMENT_TOKEN%
+setx AZ_BATCH_SOFTWARE_ENTITLEMENT_TOKEN %AZ_BATCH_SOFTWARE_ENTITLEMENT_TOKEN% /M
 
-start /wait "vrayspawner2021" "C:\Autodesk\3dsMax2021\vrayspawner2021.exe" "/i" "-port=%VRAY_PORT%" > vrayexe.output.txt
+start /wait "vrayspawner2021" "C:\Autodesk\3dsMax2021\vrayspawner2021.exe" "-port=%VRAY_PORT%" > vrayexe.output.txt
 
 exit /b %errorlevel%
