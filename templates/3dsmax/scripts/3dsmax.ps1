67fdb7383fe2e69b61de8761a0aa0685c2b60f74
David Kydd <david.kydd@microsoft.com>
2021-05-12 07:15:35 +1200

    duplicate of PR #91: Fix two VRay properties which are incorrect.
    This is to fix Issue #90

--- templates/3dsmax/scripts/3dsmax.ps1
+++ templates/3dsmax/scripts/3dsmax.ps1
@@ -103,8 +103,8 @@ if ($renderer -like "vray*")
     $outputPath = "$env:AZ_BATCH_TASK_WORKING_DIR\images\" -replace "\\", "\\"
     $vrayLogFile = "$env:AZ_BATCH_TASK_WORKING_DIR\VRayLog.log" -replace "\\", "\\"
 
-    $pre_render_script_content += "r.system_vrayLog_level = 4`r`n"
-    $pre_render_script_content += "r.system_vrayLog_file = ""$vrayLogFile""`r`n"
+    $pre_render_script_content += "r.V_Ray_settings.system_vrayLog_level = 4`r`n"
+    $pre_render_script_content += "r.V_Ray_settings.system_vrayLog_file = ""$vrayLogFile""`r`n"
     $pre_render_script_content += "indexVray5 = findString rendererName ""V_Ray_5_""`r`n"
     $pre_render_script_content += "if (indexVray5 == 1 and r.output_splitgbuffer and r.output_splitfilename != """") then (fileName = ""$outputPath"" + (filenameFromPath r.output_splitfilename); r.output_splitfilename = fileName)`r`n"
     $pre_render_script_content += "if (indexVray5 == 1 and r.output_saveRawFile and r.output_rawFileName != """") then (fileName = ""$outputPath"" + (filenameFromPath r.output_rawFileName); r.output_rawFileName = fileName)`r`n"       
