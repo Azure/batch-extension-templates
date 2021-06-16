3a923c9e370aa023f052ad56ad335f9ca7d14692
David Kydd <david.kydd@microsoft.com>
2021-06-17 10:11:14 +1200

    initial effort at corona6 with new appPackage format

--- templates/3dsmax/scripts/install-corona.ps1
+++ templates/3dsmax/scripts/install-corona.ps1
@@ -1,8 +1,7 @@
 ﻿param (
     [string]$fairSaasUsername,
     [string]$fairSaasPassword,
-    [string]$source,
-    [string]$destination
+    [string]$source
 )
 $source = $source.ToUpper()
 $current_path = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
@@ -10,19 +9,28 @@ Start-Transcript -Path (Join-Path $current_path "corona-install.log")
 cd $current_path
 Write-Host "Environment:"
 gci env: | sort name
-if (!$destination) {
-    if ($env:3DSMAX_2021) {
-        $destination = $env:3DSMAX_2021
-    } else {
-        $destination = (Get-ChildItem -Path "C:\Autodesk\3DsMax*" | Select-Object -Last 1).FullName
-    }
-}
-if (Test-Path $destination -ErrorAction Continue) {
-    Write-Host "Copying files from $source to $destination"
-    xcopy $source $destination /Y /S
+
+
+$3dsmax2021Source = "$source/Autodesk/3dsMax 2021 Plugin"
+$3dsmax2021Destination = $env:3DSMAX_2021
+if (Test-Path $3dsmax2021Destination -ErrorAction Continue) {
+    Write-Host "Copying files from $3dsmax2021Source to $3dsmax2021Destination"
+    xcopy $3dsmax2021Source $3dsmax2021Destination /Y /S
 } else {
-    Write-Host "Destination $destination not found"
+    Write-Host "Destination $3dsmax2021Destination not found"
 }
+
+$coronaProgramFilesSource = "$source/Corona"
+$coronaProgramFilesDestination = "%PROGRAMFILES%/Corona"
+
+new-item $coronaProgramFilesDestination -itemtype directory -force
+if (Test-Path $coronaProgramFilesDestination -ErrorAction Continue) {
+    Write-Host "Copying files from $coronaProgramFilesSource to $coronaProgramFilesDestination"
+    xcopy $coronaProgramFilesSource $coronaProgramFilesDestination /Y /S
+} else {
+    Write-Host "Destination $coronaProgramFilesDestination not found"
+}
+
 new-item $env:LOCALAPPDATA/CoronaRenderer -itemtype directory -force
 $fairSaasUsername + ":" + $fairSaasPassword > $env:LOCALAPPDATA/CoronaRenderer/CoronaActivation.txt
 Stop-Transcript
